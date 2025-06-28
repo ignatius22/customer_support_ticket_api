cfb35891667dc3a8772947fb03bef76451b92a5c
ignatius22 <igee54077@gmail.com>
2025-06-28 15:26:56 +0100

    Fix: fix mismatch decode

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -33,7 +33,7 @@ module Mutations
         user_id: user.id,
         exp: 24.hours.from_now.to_i
       }
-      JWT.encode(payload, ENV.fetch("RAILS_MASTER_KEY"))
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
   end
 end
