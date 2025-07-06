171b06480f4a65b1e7b5aa4a7933e9aabb0310db
ignatius22 <igee54077@gmail.com>
2025-07-06 13:07:18 +0100

    Fixed issues

--- app/graphql/types/exported_csv_type.rb
+++ app/graphql/types/exported_csv_type.rb
@@ -6,11 +6,14 @@ module Types
     def download_url
       return nil unless object.file.attached?
 
+      host = ENV.fetch("APP_HOST_PROD") { raise "APP_HOST_PROD not set" }
+      protocol = ENV.fetch("APP_PROTOCOL_PROD", "https")
 
-
-      def file_urls
-        object.files.map { |file| file.blob.url }
-      end
+      Rails.application.routes.url_helpers.rails_blob_url(
+        object.file,
+        host: host,
+        protocol: protocol
+      )
     end
   end
 end
