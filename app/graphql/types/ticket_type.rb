432368ec241daf7acd00fdb5e6c08fd726f7735d
ignatius22 <igee54077@gmail.com>
2025-07-06 13:13:19 +0100

    Fixed issues3

--- app/graphql/types/ticket_type.rb
+++ app/graphql/types/ticket_type.rb
@@ -4,15 +4,23 @@ module Types
     field :title, String, null: false
     field :description, String, null: false
     field :status, Types::TicketStatusEnum, null: false
+
     field :customer, Types::UserType, null: false
     field :agent, Types::UserType, null: true
-    field :comments, [ Types::CommentType ], null: false
-    field :file_urls, [ String ], null: false
+    field :comments, [Types::CommentType], null: false
+    field :file_urls, [String], null: false
 
     def file_urls
+      return [] unless object.files.attached?
+
       object.files.map do |file|
-        Rails.application.routes.url_helpers.rails_blob_url(file, only_path: false)
+        Rails.application.routes.url_helpers.rails_blob_url(
+          file,
+          host: ENV.fetch("APP_HOST_PROD", "https://customersupportticketapi-production.up.railway.app"),
+          protocol: "https"
+        )
       end
     end
   end
 end
+
