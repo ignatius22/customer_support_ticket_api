cd8ca11cfe10c7b13709c02eae7c35d4c451f59c
ignatius22 <igee54077@gmail.com>
2025-07-06 03:31:20 +0100

    Fixed ticket type

--- app/graphql/types/ticket_type.rb
+++ app/graphql/types/ticket_type.rb
@@ -1,12 +1,10 @@
-# app/graphql/types/ticket_type.rb
 module Types
   class TicketType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: false
-
-    # Use the enum type defined above
     field :status, Types::TicketStatusEnum, null: false
+
 
     field :customer, Types::UserType, null: false
     field :agent, Types::UserType, null: true
@@ -14,9 +12,7 @@ module Types
     field :file_urls, [ String ], null: false
 
     def file_urls
-      object.files.map do |file|
-        Rails.application.routes.url_helpers.rails_blob_url(file, only_path: false)
-      end
+      object.files.map { |file| file.blob.url }
     end
   end
 end
