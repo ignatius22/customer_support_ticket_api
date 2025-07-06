b8881d9dfa84719a3bff6e18edf157686afbf905
ignatius22 <igee54077@gmail.com>
2025-07-06 13:11:40 +0100

    Fixed issues2

--- app/graphql/types/ticket_type.rb
+++ app/graphql/types/ticket_type.rb
@@ -4,18 +4,15 @@ module Types
     field :title, String, null: false
     field :description, String, null: false
     field :status, Types::TicketStatusEnum, null: false
-
-
     field :customer, Types::UserType, null: false
     field :agent, Types::UserType, null: true
     field :comments, [ Types::CommentType ], null: false
     field :file_urls, [ String ], null: false
 
     def file_urls
-      object.files.map { |file| file.blob.url }
+      object.files.map do |file|
+        Rails.application.routes.url_helpers.rails_blob_url(file, only_path: false)
+      end
     end
   end
 end
-
-
-
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,17 +1,24 @@
+# frozen_string_literal: true
+
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
     field :role, String, null: false
-    field :file_urls, [ String ], null: true
+    field :file_urls, [String], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-
     def file_urls
-      object.files.map { |file| file.blob.url }
+      return [] unless object.respond_to?(:files) && object.files.attached?
+
+      host = ENV.fetch("APP_HOST_PROD", "https://your-default-production-url.com")
+      protocol = ENV.fetch("APP_PROTOCOL_PROD", "https")
+
+      object.files.map do |file|
+        Rails.application.routes.url_helpers.rails_blob_url(file, host: host, protocol: protocol)
+      end
     end
   end
 end
-
